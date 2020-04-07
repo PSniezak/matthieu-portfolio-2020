b84717b5344dd90d28db0de0fb9f7ef45cc41d65
Paul Sniezak <paulsniezak@gmail.com>
2020-04-07 16:27:20 +0200

    Home: mobile showcaser && Styles: media queries mixins

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,9 @@ import { AppContext } from "context/app-context";
 
 import Loader from "components/Loader";
 import Showcaser from "components/Showcaser";
+import MobileShowcaser from "components/MobileShowcaser";
+
+import { isMobile } from "utils/is-mobile";
 
 export default class Home extends React.Component {
   static contextType = AppContext;
@@ -19,12 +22,18 @@ export default class Home extends React.Component {
     const { previous } = this.context.location;
     const { state, finishLoader } = this.props;
 
+    const showcaser = isMobile ? (
+      <MobileShowcaser state={state} />
+    ) : (
+      <Showcaser state={state} />
+    );
+
     return (
       <div className={`home`}>
         {!previous && loaderActive ? (
           <Loader finishLoader={finishLoader} />
         ) : (
-          <Showcaser state={state} />
+          showcaser
         )}
       </div>
     );
---
+++ src/components/MobileShowcaser.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+import { AppContext } from "context/app-context";
+
+import { timeline } from "utils/timeline";
+
+import { data } from "data";
+
+export default class MobileShowcaser extends React.Component {
+  static contextType = AppContext;
+
+  status = null;
+
+  constructor(props) {
+    super(props);
+
+    this.ref = React.createRef();
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.check();
+  }
+
+  componentDidUpdate(prevProps) {
+    this.check();
+  }
+
+  check() {
+    const { previous } = this.context.location;
+    const { state } = this.props;
+
+    if (previous) {
+      if (state === "exiting") {
+        this.animateOut();
+      }
+
+      if (state === "entering") {
+        this.animateIn(400);
+      }
+    } else {
+      if (state === "entered") {
+        this.animateIn(0);
+      }
+    }
+  }
+
+  animateIn(delay = 0) {
+    if (this.status === "entering") return;
+
+    this.status = "entering";
+
+    timeline({}).add({
+      targets: this.ref.current.children,
+      opacity: [0, 1],
+      translateY: [80, 0],
+      translateZ: 0,
+      easing: "easeOutQuart",
+      duration: 1000,
+      delay
+    });
+  }
+
+  animateOut() {
+    const { transitionDuration } = this.context;
+    if (this.status === "exiting") return;
+
+    this.status = "exiting";
+
+    let duration = transitionDuration;
+
+    timeline({}).add({
+      targets: this.ref.current,
+      opacity: [1, 0],
+      easing: "easeInOutQuart",
+      duration: duration
+    });
+  }
+
+  render() {
+    return (
+      <div className={`mobileshowcaser`} ref={this.ref}>
+        <div className={`mobileshowcaser__slider`}>
+          {data.projects.map((project, i) => {
+            return (
+              <Link to={`/case/${project.slug}`} key={i}>
+                {project.name}/
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
