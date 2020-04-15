c5594f56c1bebf486ccbbf8fe03fe0bd43c816f7
Paul Sniezak <paulsniezak@gmail.com>
2020-04-15 13:40:38 +0200

    General: prelaod fonts && Home desktop showcase done

---
+++ src/components/Holder.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+
+export default class Holder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      holding: false,
+      start: 0,
+      ended: "begin",
+      clickEvent: null
+    };
+
+    this._timer = null;
+    this._unmounted = false;
+
+    this.start = this.start.bind(this);
+    this.end = this.end.bind(this);
+    this.interval = this.interval.bind(this);
+    this.clickCapture = this.clickCapture.bind(this);
+  }
+
+  componentWillUnmount() {
+    this._unmounted = true;
+    clearInterval(this._timer);
+    this._timer = null;
+  }
+
+  /*Start callback*/
+  start(e) {
+    let ended = this.state.ended;
+    let start = Date.now();
+    let eCopy = Object.assign({}, e);
+    eCopy.type = "Holder";
+    this.setState({
+      start: start,
+      holding: true,
+      ended: false,
+      clickEvent: eCopy,
+      isEnough: false
+    });
+    let rightNumber = this.props.time && this.props.time > 0;
+    let time = rightNumber ? this.props.time : 2;
+    if (!rightNumber) {
+      console.warn(
+        "You have specified an unvalid time prop for Holder. You need to specify a number > 0. Default time is 2."
+      );
+    }
+    if (ended) {
+      this._timer = setInterval(
+        function() {
+          this.interval(start);
+        }.bind(this),
+        time * 1000 + 1
+      );
+    }
+    if (this.props.onStart) {
+      this.props.onStart(e);
+    }
+    document.documentElement.addEventListener("mouseup", this.end);
+  }
+
+  /*End callback*/
+  end(e) {
+    document.documentElement.removeEventListener("mouseup", this.end);
+    if (this.state.ended || this._unmounted) {
+      return false;
+    }
+    let endTime = Date.now(); //End time
+    let minDiff = this.props.time * 1000; // In seconds
+    let startTime = this.state.start; // Start time
+    let diff = endTime - startTime; // Time difference
+    let isEnough = diff >= minDiff; // It has been held for enough time
+
+    this.setState({
+      holding: false,
+      ended: true,
+      clickEvent: null,
+      isEnough: isEnough
+    });
+    if (this.props.onEnd) {
+      this.props.onEnd(e, isEnough);
+    }
+  }
+
+  clickCapture(e) {
+    if (this.state.isEnough) e.stopPropagation();
+  }
+
+  /*Interval callback*/
+  interval(start) {
+    if (!this.state.ended && start === this.state.start) {
+      if (this.props.onHolder) {
+        this.props.onHolder(start, this.state.clickEvent);
+        this.setState({ holding: false });
+        return;
+      }
+    }
+  }
+
+  render() {
+    let classList = this.props.className ? this.props.className + " " : " ";
+    classList += this.state.holding ? "holder_holding " : "";
+    classList += this.state.ended ? "holder_ended " : "";
+
+    return (
+      <div
+        style={this.props.style}
+        className={classList}
+        onMouseDown={this.start}
+        onTouchStart={this.start}
+        onMouseUp={this.end}
+        onClickCapture={this.clickCapture}
+        onTouchCancel={this.end}
+        onTouchEnd={this.end}
+        ref={this.props.setRef}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
--- src/components/Showcaser.js
+++ src/components/Showcaser.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Link } from "@reach/router";
+import anime from "animejs";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { AppContext } from "context/app-context";
+
+import Holder from "components/Holder";
 
 import { timeline } from "utils/timeline";
 
@@ -11,17 +18,31 @@ export default class Showcaser extends React.Component {
   static contextType = AppContext;
 
   status = null;
+  tl = null;
+  timeToHold = 2500;
+  holdTimeline = null;
+  rowTimeline = null;
 
   constructor(props) {
     super(props);
 
     this.ref = React.createRef();
+    this.slider = React.createRef();
+    this.titles = data.projects.map(el => {
+      return React.createRef();
+    });
+    this.backgroundVideo = React.createRef();
+    this.hold = React.createRef();
+    this.holdCircle = React.createRef();
 
-    this.state = {};
+    this.state = {
+      current: 0
+    };
   }
 
   componentDidMount() {
     this.check();
+    this.init();
   }
 
   componentDidUpdate(prevProps) {
@@ -45,6 +66,15 @@ export default class Showcaser extends React.Component {
         this.animateIn(0);
       }
     }
+  }
+
+  init() {
+    document.querySelectorAll(
+      ".slick-slide[data-index='0'] a"
+    )[0].style.opacity = 1;
+
+    this.holdCircle.current.style.strokeDasharray = Math.PI * 2 * 56;
+    this.holdCircle.current.style.strokeDashoffset = Math.PI * 2 * 56;
   }
 
   animateIn(delay = 0) {
@@ -79,17 +109,430 @@ export default class Showcaser extends React.Component {
     });
   }
 
+  startHold = (e, loop = false) => {
+    console.log("START");
+    console.log(loop);
+
+    if (!loop) {
+      this.animateRows();
+    }
+
+    this.holdTimeline = anime
+      .timeline({})
+      // Hold Button
+      .add(
+        {
+          targets: this.hold.current,
+          scale: 0.85,
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      .add(
+        {
+          targets: this.holdCircle.current,
+          strokeDashoffset: [anime.setDashoffset, 0],
+          duration: this.timeToHold,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      // Background Video
+      .add(
+        {
+          targets: this.backgroundVideo.current,
+          opacity: 0,
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      // Slideshow
+      .add(
+        {
+          targets: document.querySelectorAll(".row"),
+          opacity: 1,
+          // scale: [1.1, 1],
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      // Titles around
+      .add(
+        {
+          targets: [
+            document.querySelectorAll(".slick-slide:not(.slick-current) a")
+          ],
+          opacity: 1,
+          scale: 0.9,
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      // Main Title
+      .add(
+        {
+          targets: [document.querySelectorAll(".slick-slide.slick-current a")],
+          scale: 0.9,
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      );
+  };
+
+  renderDefaultPosition = () => {
+    anime
+      // Hold Button
+      .timeline({})
+      .add(
+        {
+          targets: this.hold.current,
+          scale: 1,
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      .add(
+        {
+          targets: this.holdCircle.current,
+          // opacity: 0,
+          strokeDashoffset: anime.setDashoffset,
+          duration: this.timeToHold,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      // Background Video
+      .add(
+        {
+          targets: this.backgroundVideo.current,
+          opacity: 1,
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      .add(
+        {
+          targets: document.querySelectorAll(".row"),
+          opacity: 0,
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      // Titles around
+      .add(
+        {
+          targets: [
+            document.querySelectorAll(".slick-slide:not(.slick-current) a")
+          ],
+          opacity: 0,
+          scale: 1,
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      )
+      // Main Title
+      .add(
+        {
+          targets: [document.querySelectorAll(".slick-slide.slick-current a")],
+          scale: [0.9, 1],
+          duration: this.timeToHold / 3,
+          easing: "easeOutCubic"
+        },
+        0
+      );
+  };
+
+  switchSlides = () => {
+    let previous =
+      this.state.current === 0
+        ? data.projects.length - 1
+        : this.state.current - 1;
+
+    anime({
+      targets: document.querySelectorAll(`.row img[data-index='${previous}']`),
+      opacity: 0,
+      easing: "easeOutCubic",
+      duration: this.timeToHold / 2
+    });
+
+    anime({
+      targets: document.querySelectorAll(
+        `.row img[data-index='${this.state.current}']`
+      ),
+      opacity: 1,
+      easing: "easeOutCubic",
+      duration: this.timeToHold / 2
+    });
+  };
+
+  animateRows = () => {
+    // anime({
+    //   targets: document.querySelectorAll(`.row:nth-child(1n)`),
+    //   easing: "linear",
+    //   duration: this.timeToHold / 4,
+    //   translateX: [`150px`, 0],
+    //   scale: [1.1, 1],
+    //   complete: () => {
+    //     anime({
+    //       targets: document.querySelectorAll(`.row:nth-child(1n)`),
+    //       translateX: [0, `-731px`],
+    //       loop: true,
+    //       duration: this.timeToHold * 2,
+    //       easing: "linear"
+    //     });
+    //   }
+    // });
+
+    // anime({
+    //   targets: document.querySelectorAll(`.row:nth-child(2n)`),
+    //   easing: "linear",
+    //   duration: this.timeToHold / 4,
+    //   translateX: [`-150px`, 0],
+    //   scale: [1.1, 1],
+    //   complete: () => {
+    //     anime({
+    //       targets: document.querySelectorAll(`.row:nth-child(2n)`),
+    //       translateX: [0, `731px`],
+    //       loop: true,
+    //       duration: this.timeToHold * 2,
+    //       easing: "linear"
+    //     });
+    //   }
+    // });
+
+    // anime({
+    //   targets: document.querySelectorAll(`.row:nth-child(1n)`),
+    //   easing: "linear",
+    //   scale: {
+    //     value: [1.1, 1],
+    //     duration: this.timeToHold / 4
+    //   },
+    //   translateX: {
+    //     value: [0, `-731px`],
+    //     loop: true,
+    //     duration: this.timeToHold * 2
+    //   }
+    // });
+
+    // anime({
+    //   targets: document.querySelectorAll(`.row:nth-child(2n)`),
+    //   easing: "linear",
+    //   scale: {
+    //     value: [1.1, 1],
+    //     duration: this.timeToHold / 4
+    //   },
+    //   translateX: {
+    //     value: [0, `731px`],
+    //     loop: true,
+    //     duration: this.timeToHold * 2
+    //   }
+    // });
+
+    anime({
+      targets: document.querySelectorAll(`.row:nth-child(1n)`),
+      translateX: [0, `-731px`],
+      loop: true,
+      duration: this.timeToHold * 2,
+      easing: "linear"
+    });
+    anime({
+      targets: document.querySelectorAll(`.row:nth-child(2n)`),
+      translateX: [0, `731px`],
+      loop: true,
+      duration: this.timeToHold * 2,
+      easing: "linear"
+    });
+  };
+
+  endHold = (e, enough) => {
+    if (enough) {
+      console.log("Released after enough time");
+      this.renderDefaultPosition();
+    } else {
+      this.holdTimeline.reverse();
+      console.log("Released too soon");
+    }
+  };
+
+  onHolder = e => {
+    console.log("triggered");
+    let current =
+      this.state.current === data.projects.length - 1
+        ? 0
+        : this.state.current + 1;
+    this.setState({ current });
+    this.switchSlides();
+    this.slider.slickNext();
+    this.startHold(e, true);
+  };
+
   render() {
+    let { current } = this.state;
+    let settings = {
+      dots: false,
+      infinite: true,
+      speed: this.timeToHold / 2,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+      accessibility: false,
+      arrows: false,
+      centerPadding: "0px",
+      draggable: false,
+      swipe: false,
+      touchMove: false,
+      useCSS: true,
+      useTransform: true
+    };
+
+    const slideshows = (
+      <div>
+        <div className={"row"}>
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[1]} alt="" />
+            ))}
+          </div>
+
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[2]} alt="" />
+            ))}
+          </div>
+
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[0]} alt="" />
+            ))}
+          </div>
+        </div>
+        <div className={"row"}>
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[0]} alt="" />
+            ))}
+          </div>
+
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[1]} alt="" />
+            ))}
+          </div>
+
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[2]} alt="" />
+            ))}
+          </div>
+        </div>
+        <div className={"row"}>
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[3]} alt="" />
+            ))}
+          </div>
+
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[4]} alt="" />
+            ))}
+          </div>
+
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[5]} alt="" />
+            ))}
+          </div>
+        </div>
+        <div className={"row"}>
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[4]} alt="" />
+            ))}
+          </div>
+
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[5]} alt="" />
+            ))}
+          </div>
+
+          <div>
+            {data.projects.map((project, i) => (
+              <img key={i} data-index={i} src={project.slideshow[3]} alt="" />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+
     return (
       <div className={`showcaser`} ref={this.ref}>
         <div className={`showcaser__slider`}>
-          {data.projects.map((project, i) => {
-            return (
-              <Link to={`/case/${project.slug}`} key={i}>
-                {project.name}/
-              </Link>
-            );
-          })}
+          <div className={`showcaser__slider__titles`}>
+            <Slider {...settings} ref={ref => (this.slider = ref)}>
+              {data.projects.map((project, i) => {
+                return (
+                  <Link
+                    to={`/case/${project.slug}`}
+                    key={i}
+                    ref={this.titles[i]}
+                  >
+                    <span>{`0${i + 1}`}</span>
+                    <h2 data-title={`${project.name}/`}>{project.name}/</h2>
+                  </Link>
+                );
+              })}
+            </Slider>
+          </div>
+          <div
+            className={`showcaser__slider__background`}
+            ref={this.backgroundVideo}
+          >
+            <video
+              src={data.projects[current].mainVideo}
+              type={"video/mp4"}
+              autoPlay
+              loop
+              muted
+            ></video>
+          </div>
+          <div className={`showcaser__slider__slideshow`}>{slideshows}</div>
+        </div>
+        <div className={`showcaser__nav`}>
+          <Holder
+            time={this.timeToHold / 1000}
+            onStart={this.startHold}
+            onHolder={this.onHolder}
+            onEnd={this.endHold}
+            setRef={this.hold}
+          >
+            <span>
+              Hold <br /> to navigate
+            </span>
+            <svg height="120" width="120">
+              <circle cx="60" cy="60" r="56" strokeWidth="2" fillOpacity="0" />
+            </svg>
+            <svg height="120" width="120">
+              <circle
+                cx="60"
+                cy="60"
+                r="56"
+                strokeWidth="2"
+                fillOpacity="0"
+                ref={this.holdCircle}
+              />
+            </svg>
+          </Holder>
         </div>
       </div>
     );
